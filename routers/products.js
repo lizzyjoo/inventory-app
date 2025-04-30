842cad5feb4669933224d529064cb2aae97ca867
lizzyjoo <jejviolinist@gmail.com>
2025-04-30 15:31:01 -0400

    add search route

--- routers/products.js
+++ routers/products.js
@@ -5,4 +5,9 @@ const productController = require("../controllers/productController");
 
 router.route("/").get(productController.getAllProducts);
 
+// search
+router.route("/search").get(productController.search);
+
+// new product
+
 module.exports = router;
