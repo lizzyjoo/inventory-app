a69275c566ea85c02d6881d3c31aa7ea0f2713aa
lizzyjoo <jejviolinist@gmail.com>
2025-04-30 15:40:55 -0400

    ypdate search function to be get method

--- controllers/productController.js
+++ controllers/productController.js
@@ -10,6 +10,29 @@ async function getAllProducts(req, res) {
   });
 }
 
+async function showProduct(req, res) {
+  const id = req.params.id;
+  const products = await db.filterById(id);
+
+  res.render("viewProduct", { product: products });
+}
+
+// use get, data is appended to the URL as query parameters
+async function search(req, res) {
+  console.log("Search function called");
+  console.log("Query parameters:", req.query);
+
+  const query = req.query.query; // Using req.query instead of req.body
+  if (!query) {
+    return res.render("search", { title: "Search", query: "", products: [] });
+  }
+
+  const results = await db.search(query);
+  console.log("Search results:", results);
+
+  res.render("search", { title: "Search", query, products: results });
+}
 module.exports = {
   getAllProducts,
+  search,
 };
